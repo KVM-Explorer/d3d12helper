30e147c4ec4a5996ee84f6cc8cc766d953a52d6d
KVM-Explorer <wfcyywh1225@126.com>
2024-09-21 10:33:43 +0800

    更新构建方式

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,11 @@
 set_languages("cxx23")
 
 target("d3d12helper")
-    set_kind("static")
+    if is_mode("release") then
+        set_kind("static")
+    else
+        set_kind("shared")
+    end 
     add_includedirs("include",{public=true})
     add_files("src/*.cpp")
     add_syslinks("d3d12", "dxgi", "d3dcompiler", "dxguid")
